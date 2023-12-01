51d008fd479c6e9a85bdd373e3da050dfd0079b9
Jacob Place <jake.place@gmail.com>
2023-12-01 15:56:56 -0500

    Python solution

---
+++ day1/day1.py
@@ -0,0 +1,45 @@
+import re
+
+swap = {
+    "one": "o1e",
+    "two": "t2o",
+    "three": "t3e",
+    "four": "f4r",
+    "five": "f5e",
+    "six": "s6x",
+    "seven": "s7n",
+    "eight": "e8t",
+    "nine": "n9e",
+}
+
+
+def extract_numbers(s: str) -> str:
+    return "".join([n for n in re.findall(r"(\d)", s)])
+
+
+def convert_numbers(s: str) -> str:
+    for key in swap.keys():
+        s = s.replace(key, swap[key])
+    return s
+
+
+def score(s: str) -> int:
+    try:
+        return int(s[0] + s[-1])
+    except:
+        raise ValueError
+
+
+def main():
+    s1 = 0
+    s2 = 0
+    with open("input") as ifile:
+        for line in ifile:
+            s1 += score(extract_numbers(line))
+            s2 += score(extract_numbers(convert_numbers(line)))
+    print(f"s1:\t{s1}")
+    print(f"s2:\t{s2}")
+
+
+if __name__ == "__main__":
+    main()
