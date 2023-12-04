d7923373fb72d5a5961ad3b4f6779c1113401c1e
Jake Place <jplace@censys.com>
2023-12-04 09:57:44 -0500

    part 2

--- day4/day4.py
+++ day4/day4.py
@@ -1,18 +1,22 @@
 import re
 
-with open('test') as ifile:
+with open('input') as ifile:
     points = 0
-    cards = {}
+    cards = list()
     for line in ifile:
         card, line = line.split(':')
-        card = [int(m) for m in re.findall(r'\d+', card)][0]
         win, hand = line.split('|')
         win = set([int(m) for m in re.findall(r'\d+', win)])
         hand = set([int(m) for m in re.findall(r'\d+', hand)])
         match = win.intersection(hand)
-        cards[card] = list(match)
+        cards.append((1, len(match)))
         if match:
             points += 2 ** (len(match) - 1)
+total = 0
+for i in range(len(cards)):
+    for j in range(cards[i][1]):
+        cards[j+i+1] = (cards[j+i+1][0] + cards[i][0], cards[j+i+1][1])
+for card in cards:
+    total += card[0]
 print(points)
-
-#p2
+print(total)
